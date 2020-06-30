09407dcce2a8ffb55ce7874da3e2f30cfb5260a7
cd606 <64788159+cd606@users.noreply.github.com>
2020-06-30 10:41:51 -0500

    add new overall headers

---
+++ tm_kit/basic/transaction/TransactionClient.hpp
@@ -0,0 +1,16 @@
+#ifndef TM_KIT_BASIC_TRANSACTION_TI_TRANSACTION_TRANSACTION_CLIENT_HPP_
+#define TM_KIT_BASIC_TRANSACTION_TI_TRANSACTION_TRANSACTION_CLIENT_HPP_
+
+#include <tm_kit/basic/transaction/v2/TransactionInterface.hpp>
+#include <tm_kit/basic/transaction/v2/DataStreamInterface.hpp>
+#include <tm_kit/basic/transaction/v2/TransactionDataStore.hpp>
+#include <tm_kit/basic/transaction/v2/GeneralSubscriber.hpp>
+#include <tm_kit/basic/transaction/v2/DefaultUtilities.hpp>
+#include <tm_kit/basic/transaction/v2/TransactionFacility.hpp>
+#include <tm_kit/basic/transaction/v2/TransactionDeltaMerger.hpp>
+
+namespace dev { namespace cd606 { namespace tm { namespace basic { namespace transaction {
+    namespace current = v2;
+} } } } }
+
+#endif
---
+++ tm_kit/basic/transaction/TransactionServer.hpp
@@ -0,0 +1,7 @@
+#ifndef TM_KIT_BASIC_TRANSACTION_TI_TRANSACTION_TRANSACTION_SERVER_HPP_
+#define TM_KIT_BASIC_TRANSACTION_TI_TRANSACTION_TRANSACTION_SERVER_HPP_
+
+#include <tm_kit/basic/transaction/v2/TransactionLogicCombination.hpp>
+#include <tm_kit/basic/transaction/TransactionClient.hpp>
+
+#endif
