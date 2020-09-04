cc50454a4b8eade49021bd4ed60b9c5bef7b9dcf
cd606 <64788159+cd606@users.noreply.github.com>
2020-09-04 10:43:46 -0500

    more updates on shared chain (work in progress)

--- tm_kit/basic/simple_shared_chain/ChainReader.hpp
+++ tm_kit/basic/simple_shared_chain/ChainReader.hpp
@@ -1,49 +1,37 @@
 #ifndef TM_KIT_BASIC_SIMPLE_SHARED_CHAIN_CHAIN_READER_HPP_
 #define TM_KIT_BASIC_SIMPLE_SHARED_CHAIN_CHAIN_READER_HPP_
 
-#include <tm_kit/infra/RealTimeApp.hpp>
-#include <chrono>
-#include <thread>
+#include <tm_kit/basic/VoidStruct.hpp>
+#include <optional>
 
 namespace dev { namespace cd606 { namespace tm { namespace basic { namespace simple_shared_chain {
     template <class Env, class Chain, class ChainItemFolder>
-    class ChainReader : public infra::RealTimeApp<Env>::template AbstractImporter<typename ChainItemFolder::ResultType> {
+    class ChainReader {
     private:
         Chain const *chain_;
         typename Chain::ItemType currentItem_;
         ChainItemFolder folder_;
-        std::chrono::system_clock::duration checkPeriod_;
         typename ChainItemFolder::ResultType currentValue_;
-        std::thread th_;
-        std::atomic<bool> running_;
-
-        virtual typename ChainItemFolder::ResultType fetchInitialValue(Env *) = 0;
-        void run(Env *env) {
-            while (running_) {
-                bool hasNew = false;
-                while (chain_->fetchNext(&currentItem_)) {
-                    currentValue_ = folder_.fold(currentValue_, currentItem_);
-                    hasNew = true;
-                }
-                if (hasNew) {
-                    this->publish(env, typename ChainItemFolder::ResultType {currentValue_});
-                }
-                std::this_thread::sleep_for(checkPeriod_);
-            }
-        }
     public:
-        ChainReader(Chain const *chain, std::chrono::system_clock::duration const &checkPeriod) : chain_(chain), currentItem_(chain->initialItem()), folder_(), checkPeriod_(checkPeriod), currentValue_(), th_(), running_(false) {}
-        virtual ~ChainReader() {
-            if (running_) {
-                running_ = false;
-                th_.join();
-            }
+        ChainReader(Env *env, Chain const *chain) : chain_(chain), currentItem_(), folder_(), currentValue_() {
+            currentValue_ = folder_.initialize(env);
         }
-        virtual void start(Env *env) override final {
-            currentValue_ = fetchInitialValue(env);
-            running_ = true;
-            th_ = std::thread(&ChainReader::run, this, env);
-            th_.detach();
+        ~ChainReader() = default;
+        ChainReader(ChainReader const &) = delete;
+        ChainReader &operator=(ChainReader const &) = delete;
+        ChainReader(ChainReader &&) = default;
+        ChainReader &operator=(ChainReader &&) = default;
+        typename std::optional<typename ChainItemFolder::ResultType> operator()(VoidStruct &&) {
+            bool hasNew = false;
+            while (chain_->fetchNext(&currentItem_)) {
+                currentValue_ = folder_.fold(currentValue_, currentItem_);
+                hasNew = true;
+            }
+            if (hasNew) {
+                return {currentValue_};
+            } else {
+                return std::nullopt;
+            }
         }
     };
 } } } } }
--- tm_kit/basic/simple_shared_chain/ChainWriter.hpp
+++ tm_kit/basic/simple_shared_chain/ChainWriter.hpp
@@ -2,22 +2,28 @@
 #define TM_KIT_BASIC_SIMPLE_SHARED_CHAIN_CHAIN_WRITER_HPP_
 
 #include <tm_kit/infra/RealTimeApp.hpp>
+#include <tm_kit/infra/SinglePassIterationApp.hpp>
 #include <chrono>
 #include <thread>
 #include <mutex>
 #include <condition_variable>
 
 namespace dev { namespace cd606 { namespace tm { namespace basic { namespace simple_shared_chain {
+    template <class App, class Chain, class ChainItemFolder, class InputHandler>
+    class ChainWriter {};
+
     template <class Env, class Chain, class ChainItemFolder, class InputHandler>
-    class ChainWriter : public infra::RealTimeApp<Env>::IExternalComponent, public infra::RealTimeApp<Env>::template ThreadedHandler<typename infra::RealTimeApp<Env>::template Key<typename InputHandler::InputType>>, public infra::RealTimeApp<Env>::template AbstractOnOrderFacility<typename InputHandler::InputType, typename InputHandler::ResponseType> {
+    class ChainWriter<typename infra::RealTimeApp<Env>, Chain, ChainItemFolder, InputHandler>
+        : 
+        public infra::RealTimeApp<Env>::IExternalComponent
+        , public infra::RealTimeApp<Env>::template ThreadedHandler<typename infra::RealTimeApp<Env>::template Key<typename InputHandler::InputType>>
+        , public infra::RealTimeApp<Env>::template AbstractOnOrderFacility<typename InputHandler::InputType, typename InputHandler::ResponseType> {
     private:
         Chain const *chain_;
         typename Chain::ItemType currentItem_;
         ChainItemFolder folder_;
         InputHandler inputHandler_;
         typename ChainItemFolder::ResultType currentState_;
-
-        virtual typename ChainItemFolder::ResultType fetchInitialState(Env *) = 0;
     protected:
         virtual void idleWork() override final {
             if (chain_->fetchNext(&currentItem_)) {
@@ -35,23 +41,93 @@ namespace dev { namespace cd606 { namespace tm { namespace basic { namespace sim
                     if (chain_->append(&currentItem_, itemToBeWritten)) {
                         this->publish(data.environment, typename infra::RealTimeApp<Env>::template Key<typename InputHandler::ResponseType> {
                             data.timedData.value.id(), std::move(response)
-                        });
+                        }, true);
                         break;
                     }
                 } else {
                     this->publish(data.environment, typename infra::RealTimeApp<Env>::template Key<typename InputHandler::ResponseType> {
                         data.timedData.value.id(), std::move(response)
-                    });
+                    }, true);
                     break;
                 }
             }
         }
     public:
-        ChainWriter(Chain const *chain) : infra::RealTimeApp<Env>::template ThreadedHandler<typename infra::RealTimeApp<Env>::template Key<typename InputHandler::InputType>>(), chain_(chain), currentItem_(chain->initialItem()), folder_(), inputHandler_(), currentState_() {}
+        ChainWriter(Chain const *chain) : 
+            infra::RealTimeApp<Env>::IExternalComponent()
+            , infra::RealTimeApp<Env>::template ThreadedHandler<typename infra::RealTimeApp<Env>::template Key<typename InputHandler::InputType>>()
+            , infra::RealTimeApp<Env>::template AbstractOnOrderFacility<typename InputHandler::InputType, typename InputHandler::ResponseType>()
+            , chain_(chain)
+            , currentItem_(chain->initialItem())
+            , folder_()
+            , inputHandler_()
+            , currentState_() 
+        {}
         virtual ~ChainWriter() {
         }
+        ChainWriter(ChainWriter const &) = delete;
+        ChainWriter &operator=(ChainWriter const &) = delete;
+        ChainWriter(ChainWriter &&) = default;
+        ChainWriter &operator=(ChainWriter &&) = default;
         virtual void start(Env *env) override final {
-            currentState_ = fetchInitialValue(env);
+            currentState_ = folder_.initialize(env);
+            inputHandler_.initialize(env);
+        }
+    };
+
+    template <class Env, class Chain, class ChainItemFolder, class InputHandler>
+    class ChainWriter<typename infra::SinglePassIterationApp<Env>, Chain, ChainItemFolder, InputHandler>
+        : 
+        public infra::SinglePassIterationApp<Env>::IExternalComponent
+        , public infra::SinglePassIterationApp<Env>::template AbstractOnOrderFacility<typename InputHandler::InputType, typename InputHandler::ResponseType> {
+    private:
+        Chain const *chain_;
+        typename Chain::ItemType currentItem_;
+        ChainItemFolder folder_;
+        InputHandler inputHandler_;
+        typename ChainItemFolder::ResultType currentState_;
+    protected:
+        virtual void handle(typename infra::SinglePassIterationApp<Env>::template Data<typename infra::RealTimeApp<Env>::template Key<typename InputHandler::InputType>> &&data) override final {
+            while (true) {
+                while (chain_->fetchNext(&currentItem_)) {
+                    currentState_ = folder_.fold(currentState_, currentItem_);
+                }
+                typename Chain::ItemType itemToBeWritten;
+                typename InputHandler::ResponseType response;
+                if (inputHandler_.handleInput(data.timedData.value.key(), currentState_, &itemToBeWritten, &response)) {
+                    if (chain_->append(&currentItem_, itemToBeWritten)) {
+                        this->publish(data.environment, typename infra::RealTimeApp<Env>::template Key<typename InputHandler::ResponseType> {
+                            data.timedData.value.id(), std::move(response)
+                        }, true);
+                        break;
+                    }
+                } else {
+                    this->publish(data.environment, typename infra::RealTimeApp<Env>::template Key<typename InputHandler::ResponseType> {
+                        data.timedData.value.id(), std::move(response)
+                    }, true);
+                    break;
+                }
+            }
+        }
+    public:
+        ChainWriter(Chain const *chain) :
+            infra::SinglePassIterationApp<Env>::IExternalComponent()
+            , infra::SinglePassIterationApp<Env>::template AbstractOnOrderFacility<typename InputHandler::InputType, typename InputHandler::ResponseType>()
+            , chain_(chain)
+            , currentItem_(chain->initialItem())
+            , folder_()
+            , inputHandler_()
+            , currentState_() 
+        {}
+        virtual ~ChainWriter() {
+        }
+        ChainWriter(ChainWriter const &) = delete;
+        ChainWriter &operator=(ChainWriter const &) = delete;
+        ChainWriter(ChainWriter &&) = default;
+        ChainWriter &operator=(ChainWriter &&) = default;
+        virtual void start(Env *env) override final {
+            currentState_ = folder_.initialize(env);
+            inputHandler_.initialize(env);
         }
     };
 } } } } }
