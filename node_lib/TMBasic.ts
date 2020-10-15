fe326854c7ecfc98af2cc0d466b6e8d478127aed
cd606 <64788159+cd606@users.noreply.github.com>
2020-10-15 16:01:14 -0500

    more node helper code changes

--- node_lib/TMBasic.ts
+++ node_lib/TMBasic.ts
@@ -1,81 +1,216 @@
 import * as TMInfra from '../../tm_infra/node_lib/TMInfra'
 import * as _ from 'lodash';
+import * as dateformat from 'dateformat'
+
+export interface ClockSettings {
+    synchronizationPointActual : Date;
+    synchronizationPointVirtual : Date;
+    speed : number;
+}
+export interface ClockSettingsDescription {
+    clock_settings : {
+        actual : {
+            time : string; //"HH:MM"
+        }
+        , virtual : {
+            date : string; //"YYYY-mm-dd"
+            time : string; //"HH:MM"
+        }
+        , speed : number; 
+    }
+}
+export function parseClockSettingsDescription(d : ClockSettingsDescription) : ClockSettings {
+    if (!/^\d{2}:\d{2}$/.test(d.clock_settings.actual.time)) {
+        return null;
+    }
+    if (!/^\d{4}-\d{2}-\d{2}$/.test(d.clock_settings.virtual.date)) {
+        return null;
+    }
+    if (!/^\d{2}:\d{2}$/.test(d.clock_settings.virtual.time)) {
+        return null;
+    }
+    let actual = new Date();
+    actual = new Date(
+        actual.getFullYear(), actual.getMonth(), actual.getDate()
+        , parseInt(d.clock_settings.actual.time.substring(0,2))
+        , parseInt(d.clock_settings.actual.time.substring(3,5))
+    );
+    let virtual = new Date(
+        parseInt(d.clock_settings.virtual.date.substring(0,4))
+        , parseInt(d.clock_settings.virtual.date.substring(5,7))
+        , parseInt(d.clock_settings.virtual.date.substring(8,10))
+        , parseInt(d.clock_settings.virtual.time.substring(0,2))
+        , parseInt(d.clock_settings.virtual.time.substring(3,5))
+    );
+    return {
+        synchronizationPointActual : actual
+        , synchronizationPointVirtual : virtual
+        , speed : d.clock_settings.speed
+    };
+}
+export class ClockEnv implements TMInfra.EnvBase {
+    private clockSettings : ClockSettings;
+    public constructor(s : ClockSettings = null) {
+        this.clockSettings = s;
+    }
+    private actualToVirtual(d : Date) : Date {
+        if (this.clockSettings == null) {
+            return d;
+        } else {
+            return new Date(
+                this.clockSettings.synchronizationPointVirtual.getTime()
+                +Math.round((d.getTime()-this.clockSettings.synchronizationPointActual.getTime())*this.clockSettings.speed)
+            );
+        }
+    }
+    public virtualDuration(actualD : number) : number {
+        if (this.clockSettings == null) {
+            return actualD;
+        } else {
+            return Math.round(actualD*this.clockSettings.speed);
+        }
+    }
+    public virtualToActual(d : Date) : Date {
+        if (this.clockSettings == null) {
+            return d;
+        } else {
+            return new Date(
+                this.clockSettings.synchronizationPointActual.getTime()
+                +Math.round((d.getTime()-this.clockSettings.synchronizationPointVirtual.getTime())*1.0/this.clockSettings.speed)
+            );
+        }
+    }
+    public actualDuration(virtualD : number) : number {
+        if (this.clockSettings == null) {
+            return virtualD;
+        } else {
+            return Math.round(virtualD*1.0/this.clockSettings.speed);
+        }
+    }
+    public now() : Date {
+        if (this.clockSettings == null) {
+            return new Date();
+        } else {
+            return this.actualToVirtual(new Date());
+        }
+    }
+    public log(level : TMInfra.LogLevel, s : string) : void {
+        console.log(`[${dateformat(this.now(), "yyyy-mm-dd HH:MM:ss.l")}] [${TMInfra.LogLevel[level]}] ${s}`);
+    }
+    public exit() : void {
+        process.exit(0);
+    }
+
+    public static clockSettingsWithStartPoint(actualHHMM : number, virtualStartPoint : Date, speed : number) : ClockSettings {
+        let d = new Date();
+        return {
+            synchronizationPointActual : new Date(d.getFullYear(), d.getMonth(), d.getDate(), Math.floor(actualHHMM/100), (actualHHMM%100))
+            , synchronizationPointVirtual : virtualStartPoint
+            , speed : speed
+        };
+    }
+    public static clockSettingsWithStartPointCorrespondingToNextAlignment(actualTimeAlignMinute : number, virtualStartPoint : Date, speed : number) : ClockSettings {
+        let d = new Date();
+        let min = d.getMinutes();
+        if (min % actualTimeAlignMinute == 0) {
+            min += actualTimeAlignMinute;
+        } else {
+            min += (min % actualTimeAlignMinute);
+        }
+        let hour = d.getHours();
+        if (min >= 60) {
+            hour = hour+1;
+            min = 0;
+            if (hour >= 24) {
+                d = new Date(d.getTime()+24*3600*1000);
+                d = new Date(d.getFullYear(), d.getMonth(), d.getDate());
+            } else {
+                d = new Date(d.getFullYear(), d.getMonth(), d.getDate(), hour, min);
+            }
+        }
+        return {
+            synchronizationPointActual : d
+            , synchronizationPointVirtual : virtualStartPoint
+            , speed : speed
+        };
+    }
+}
 
 export class ClockImporter {
-    public static createOneShotClockImporter<Env,T>(when : Date, gen : (d : Date) => T) : TMInfra.RealTimeApp.Importer<Env,T> {
+    public static createOneShotClockImporter<Env extends ClockEnv,T>(when : Date, gen : (d : Date) => T) : TMInfra.RealTimeApp.Importer<Env,T> {
         return TMInfra.RealTimeApp.Utils.simpleImporter<Env,T>(
-            function (pub : TMInfra.RealTimeApp.PubFunc<T>) : void {
-                let now = new Date();
+            function (env : Env, pub : TMInfra.RealTimeApp.PubFunc<T>) : void {
+                let now = env.now();
                 if (now <= when) {
                     setTimeout(() => {
-                        pub(gen(new Date()), true);
-                    }, when.getTime()-now.getTime());
+                        pub(gen(env.now()), true);
+                    }, env.actualDuration(when.getTime()-now.getTime()));
                 }
             }
         );
     }
-    public static createOneShotClockConstImporter<Env,T>(when : Date, t : T) : TMInfra.RealTimeApp.Importer<Env,T> {
+    public static createOneShotClockConstImporter<Env extends ClockEnv,T>(when : Date, t : T) : TMInfra.RealTimeApp.Importer<Env,T> {
         return TMInfra.RealTimeApp.Utils.simpleImporter<Env,T>(
-            function (pub : TMInfra.RealTimeApp.PubFunc<T>) : void {
-                let now = new Date();
+            function (env : Env, pub : TMInfra.RealTimeApp.PubFunc<T>) : void {
+                let now = env.now()
                 if (now <= when) {
                     setTimeout(() => {
                         pub(t, true);
-                    }, when.getTime()-now.getTime());
+                    }, env.actualDuration(when.getTime()-now.getTime()));
                 }
             }
         );
     }
-    public static createRecurringClockImporter<Env,T>(start : Date, end : Date, periodMs : number, gen : (d : Date) => T) : TMInfra.RealTimeApp.Importer<Env,T> {
+    public static createRecurringClockImporter<Env extends ClockEnv,T>(start : Date, end : Date, periodMs : number, gen : (d : Date) => T) : TMInfra.RealTimeApp.Importer<Env,T> {
         return TMInfra.RealTimeApp.Utils.simpleImporter<Env,T>(
-            function (pub : TMInfra.RealTimeApp.PubFunc<T>) : void {
+            function (env : Env, pub : TMInfra.RealTimeApp.PubFunc<T>) : void {
                 let f = () => {
-                    let d = new Date();
+                    let d = env.now();
                     let isFinal = (d.getTime()+periodMs)>end.getTime();
                     pub(gen(d), isFinal);
                     if (!isFinal) {
-                        setTimeout(f, periodMs);
+                        setTimeout(f, env.actualDuration(periodMs));
                     }
                 };
-                let now = new Date();
+                let now = env.now();
                 let realStart = start;
                 while (now > realStart) {
                     realStart.setTime(realStart.getTime()+periodMs);
                 }
                 if (realStart < end) {
-                    setTimeout(f, realStart.getTime()-now.getTime());
+                    setTimeout(f, env.actualDuration(realStart.getTime()-now.getTime()));
                 }
             }
         );
     }
-    public static createRecurringConstClockImporter<Env,T>(start : Date, end : Date, periodMs : number, t : T) : TMInfra.RealTimeApp.Importer<Env,T> {
+    public static createRecurringConstClockImporter<Env extends ClockEnv,T>(start : Date, end : Date, periodMs : number, t : T) : TMInfra.RealTimeApp.Importer<Env,T> {
         return this.createRecurringClockImporter<Env,T>(start,end,periodMs,((_d : Date) => t));
     }
-    public static createVariableDurationRecurringClockImporter<Env,T>(start : Date, end : Date, periodCalc : ((d : Date) => number), gen : (d : Date) => T) : TMInfra.RealTimeApp.Importer<Env,T> {
+    public static createVariableDurationRecurringClockImporter<Env extends ClockEnv,T>(start : Date, end : Date, periodCalc : ((d : Date) => number), gen : (d : Date) => T) : TMInfra.RealTimeApp.Importer<Env,T> {
         return TMInfra.RealTimeApp.Utils.simpleImporter<Env,T>(
-            function (pub : TMInfra.RealTimeApp.PubFunc<T>) : void {
+            function (env : Env, pub : TMInfra.RealTimeApp.PubFunc<T>) : void {
                 let f = () => {
-                    let d = new Date();
+                    let d = env.now();
                     let nextPeriod = periodCalc(d);
                     let isFinal = (d.getTime()+nextPeriod)>end.getTime();
                     pub(gen(d), isFinal);
                     if (!isFinal) {
-                        setTimeout(f, nextPeriod);
+                        setTimeout(f, env.actualDuration(nextPeriod));
                     }
                 };
-                let now = new Date();
+                let now = env.now();
                 let realStart = start;
                 let firstPeriod = periodCalc(start);
                 while (now > realStart) {
                     realStart.setTime(realStart.getTime()+firstPeriod);
                 }
                 if (realStart < end) {
-                    setTimeout(f, realStart.getTime()-now.getTime());
+                    setTimeout(f, env.actualDuration(realStart.getTime()-now.getTime()));
                 }
             }
         );
     }
-    public static createRecurringVariableDurationConstClockImporter<Env,T>(start : Date, end : Date, periodCalc : ((d : Date) => number), t : T) : TMInfra.RealTimeApp.Importer<Env,T> {
+    public static createRecurringVariableDurationConstClockImporter<Env extends ClockEnv,T>(start : Date, end : Date, periodCalc : ((d : Date) => number), t : T) : TMInfra.RealTimeApp.Importer<Env,T> {
         return this.createVariableDurationRecurringClockImporter<Env,T>(start,end,periodCalc,((_d : Date) => t));
     }
 }
@@ -85,7 +220,7 @@ export interface ClockOnOrderFacilityInput<S> {
     callbackDurations : number[];        
 }
 export class ClockOnOrderFacility {
-    public static createClockCallback<Env,S,T>(
+    public static createClockCallback<Env extends ClockEnv,S,T>(
         converter : ((d: Date, index : number, count : number) => T)
     ) : TMInfra.RealTimeApp.OnOrderFacility<Env,ClockOnOrderFacilityInput<S>,T> {
         class LocalF extends TMInfra.RealTimeApp.OnOrderFacility<Env,ClockOnOrderFacilityInput<S>,T> {
@@ -105,7 +240,7 @@ export class ClockOnOrderFacility {
                 let thisObj = this;
                 if (filteredDurations.length == 0) {
                     setImmediate(() => {
-                        let now = new Date();
+                        let now = env.now();
                         let t = thisObj.conv(now, 0, 0);
                         thisObj.publish({
                             environment : env
@@ -123,7 +258,7 @@ export class ClockOnOrderFacility {
                     let count = filteredDurations.length;
                     for (let i=0; i<count; ++i) {
                         setTimeout(() => {
-                            let now = new Date();
+                            let now = env.now();
                             let t = thisObj.conv(now, i, count);
                             thisObj.publish({
                                 environment : env
@@ -136,7 +271,7 @@ export class ClockOnOrderFacility {
                                     , finalFlag : (i == count-1)
                                 }
                             });
-                        }, filteredDurations[count]);
+                        }, env.actualDuration(filteredDurations[i]));
                     }
                 }
             }
