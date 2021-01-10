83cea91c653a5231da8b424a5782597f9d9197d0
cd606 <cd606dc@gmail.com>
2021-01-09 20:48:12 -0600

    add new methods to chain creator

--- tm_kit/basic/simple_shared_chain/InMemorySharedChainCreator.hpp
+++ tm_kit/basic/simple_shared_chain/InMemorySharedChainCreator.hpp
@@ -373,7 +373,7 @@ namespace dev { namespace cd606 { namespace tm { namespace basic { namespace sim
         }
 
         template <class ChainData, class ExtraData>
-        static void writeExtraData(Env *env, std::string const &descriptor, std::string const &key, ExtraData const &extraData) {
+        static void writeExtraData(typename App::EnvironmentType *env, std::string const &descriptor, std::string const &key, ExtraData const &extraData) {
             auto parsed = parseDescriptor(descriptor);
             if (std::get<0>(parsed)) {
                 writeExtraDataHelper<ExtraData>(
@@ -400,7 +400,7 @@ namespace dev { namespace cd606 { namespace tm { namespace basic { namespace sim
             }
         }
         template <class ChainData, class ExtraData>
-        static std::optional<ExtraData> readExtraData(Env *, std::string const &, std::string const &) {
+        static std::optional<ExtraData> readExtraData(typename App::EnvironmentType *, std::string const &, std::string const &) {
             auto parsed = parseDescriptor(descriptor);
             if (std::get<0>(parsed)) {
                 return readExtraDataHelper<ExtraData>(
