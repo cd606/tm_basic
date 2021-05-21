29ec6ed2194b4b544f9626227bba0424892f4734
cd606 <cd606dc@gmail.com>
2021-05-21 17:25:25 -0500

    node version file importer change

--- node_lib/TMBasic.ts
+++ node_lib/TMBasic.ts
@@ -762,23 +762,32 @@ export namespace Files {
                 var transform = genericRecordStream<TopicCaptureFileRecord>(new TopicCaptureFileRecordReader(this.option));
                 this.inputStream.pipe(transform);
                 (async () => {
-                    for await (const rec of transform) {
-                        var t = rec.time as Date;
-                        if (this.overrideDate) {
-                            var tStart = new Date(t.getFullYear(), t.getMonth(), t.getDate(), 0, 0, 0, 0);
-                            t = new Date(t.getTime()-tStart.getTime()+this.todayStart.getTime());
-                            rec.time = t;
-                        }
+                    while (true) {
                         var now = this.env.now();
-                        if (t < now) {
-                            if (now.getTime()-t.getTime() < 1000) {
-                                this.publish(rec, false);
+                        var hasNext = false;
+                        for await (const rec of transform) {
+                            hasNext = true;
+                            var t = rec.time as Date;
+                            if (this.overrideDate) {
+                                var tStart = new Date(t.getFullYear(), t.getMonth(), t.getDate(), 0, 0, 0, 0);
+                                t = new Date(t.getTime()-tStart.getTime()+this.todayStart.getTime());
+                                rec.time = t;
                             }
-                        } else if (rec.time.equals(now)) {
-                            this.publish(rec, false);
-                        } else {
-                            await new Promise(resolve => setTimeout(resolve, this.env.actualDuration(t.getTime()-now.getTime())));
-                            this.publish(rec, false);
+                            
+                            if (t < now) {
+                                if (now.getTime()-t.getTime() < 1000) {
+                                    this.publish(rec, false);
+                                }
+                            } else if (rec.time.equals(now)) {
+                                this.publish(rec, false);
+                            } else {
+                                await new Promise(resolve => setTimeout(resolve, this.env.actualDuration(t.getTime()-now.getTime())));
+                                this.publish(rec, false);
+                                break;
+                            }
+                        }
+                        if (!hasNext) {
+                            break;
                         }
                     }
                 })();
