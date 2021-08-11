51566d48ef1274c2585c0773d06a614a1800e666
cd606 <cd606dc@gmail.com>
2021-08-11 18:23:32 -0500

    more updates on proto-interop

--- tm_kit/basic/EqualityCheckHelper.hpp
+++ tm_kit/basic/EqualityCheckHelper.hpp
@@ -2,6 +2,7 @@
 #define TM_KIT_BASIC_EQUALITY_CHECK_HELPER_HPP_
 
 #include <ctime>
+#include <valarray>
 
 namespace dev { namespace cd606 { namespace tm { namespace basic {
     template <class T>
@@ -17,6 +18,22 @@ namespace dev { namespace cd606 { namespace tm { namespace basic {
         static bool check(std::tm const &a, std::tm const &b) {
             return (a.tm_year == b.tm_year && a.tm_mon == b.tm_mon && a.tm_mday == b.tm_mday
                 && a.tm_hour == b.tm_hour && a.tm_min == b.tm_min && a.tm_sec == b.tm_sec);
+        }
+    };
+    template <class T>
+    class EqualityCheckHelper<std::valarray<T>> {
+    public:
+        static bool check(std::valarray<T> const &a, std::valarray<T> const &b) {
+            if (a.size() == b.size()) {
+                for (std::size_t ii=0; ii<a.size(); ++ii) {
+                    if (!(a[ii] == b[ii])) {
+                        return false;
+                    }
+                }
+                return true;
+            } else {
+                return false;
+            }
         }
     };
     
