7d0bcf50c847d565b88583645317e4b7e8ad89a8
cd606 <cd606dc@gmail.com>
2022-09-06 09:20:17 -0500

    add an include

--- tm_kit/basic/SpdLoggingComponent.hpp
+++ tm_kit/basic/SpdLoggingComponent.hpp
@@ -6,6 +6,7 @@
 #include <chrono>
 #include <thread>
 #include <algorithm>
+#include <optional>
 #include <spdlog/spdlog.h>
 #include <spdlog/sinks/basic_file_sink.h>
 #include <spdlog/sinks/rotating_file_sink.h>
