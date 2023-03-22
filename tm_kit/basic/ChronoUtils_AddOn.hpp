bb8ab6b26608e32261f89c4c5db3187b7f0d8770
cd606 <cd606dc@gmail.com>
2023-03-22 03:39:11 -0500

    add zoned sinceMidnight

--- tm_kit/basic/ChronoUtils_AddOn.hpp
+++ tm_kit/basic/ChronoUtils_AddOn.hpp
@@ -7,8 +7,18 @@
 //stay in the tm::infra namespace
 namespace dev { namespace cd606 { namespace tm { namespace infra {
     namespace withtime_utils {
+        //The implementation may break down on dst-changing Sundays
         extern std::chrono::system_clock::time_point parseZonedTime(int year, int month, int day, int hour, int minute, int second, int microseconds, std::string_view const &timeZoneName);
         extern std::chrono::system_clock::time_point parseZonedTime(std::string_view const &timeString, std::string_view const &timeZoneName);
+        template <class Duration>
+        inline int64_t sinceZonedMidnight(std::chrono::system_clock::time_point const &tp, std::string_view const &timeZoneName) {
+            std::time_t t = std::chrono::system_clock::to_time_t(tp);
+            std::tm *m = std::localtime(&t);
+            auto midnight = parseZonedTime(
+                m->tm_year+1900, m->tm_mon+1, m->tm_mday, 0, 0, 0, 0, timeZoneName
+            );
+            return static_cast<int64_t>(std::chrono::duration_cast<Duration>(tp-midnight).count());
+        }
     }
 } } } }
 
