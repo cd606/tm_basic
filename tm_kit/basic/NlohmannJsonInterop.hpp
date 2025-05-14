a2f038fb4a8631f9a0e4a3992afcf6538ffebafe
cd606 <cd606dc@gmail.com>
2025-05-13 20:22:16 -0500

    add JsonVariantAutoDetect

--- tm_kit/basic/NlohmannJsonInterop.hpp
+++ tm_kit/basic/NlohmannJsonInterop.hpp
@@ -529,19 +529,22 @@ namespace dev { namespace cd606 { namespace tm { namespace basic { namespace nlo
     };
 
     struct JsonVariantTypeMark {};
+    struct JsonVariantAutoDetectTypeMark {};
 
     template <class... Ts>
     using JsonVariant = SingleLayerWrapperWithTypeMark<JsonVariantTypeMark, std::variant<std::monostate,Ts...>>;
+    template <class... Ts>
+    using JsonVariantAutoDetect = SingleLayerWrapperWithTypeMark<JsonVariantAutoDetectTypeMark, std::variant<std::monostate,Ts...>>;
 
     template <class M, class T>
-    class JsonEncoder<SingleLayerWrapperWithTypeMark<M,T>, std::enable_if_t<!std::is_same_v<M,JsonVariantTypeMark>, void>> {
+    class JsonEncoder<SingleLayerWrapperWithTypeMark<M,T>, std::enable_if_t<!std::is_same_v<M,JsonVariantTypeMark> && !std::is_same_v<M,JsonVariantAutoDetectTypeMark>, void>> {
     public:
         static void write(nlohmann::json &output, std::optional<std::string> const &key, SingleLayerWrapperWithTypeMark<M,T> const &data) {
             JsonEncoder<T>::write(output, key, data.value);
         }
     };
     template <class M, class T>
-    struct JsonWrappable<SingleLayerWrapperWithTypeMark<M,T>, std::enable_if_t<!std::is_same_v<M,JsonVariantTypeMark>, void>> {
+    struct JsonWrappable<SingleLayerWrapperWithTypeMark<M,T>, std::enable_if_t<!std::is_same_v<M,JsonVariantTypeMark> && !std::is_same_v<M,JsonVariantAutoDetectTypeMark>, void>> {
         static constexpr bool value = JsonWrappable<T>::value;
     };
 
@@ -628,6 +631,49 @@ namespace dev { namespace cd606 { namespace tm { namespace basic { namespace nlo
         }
     public:
         static void write(nlohmann::json &output, std::optional<std::string> const &key, JsonVariant<Ts...> const &data) {
+            if constexpr (sizeof...(Ts) >= 0) {
+                write_impl<0>((key?output[*key]:output), data);
+            }
+        }
+    };
+    template <class... Ts>
+    struct JsonWrappable<JsonVariantAutoDetect<Ts...>, void> {
+    private:
+        template <std::size_t Index>
+        static constexpr bool value_internal() {
+            if constexpr (Index < sizeof...(Ts)) {
+                if constexpr (!JsonWrappable<std::variant_alternative_t<Index+1,std::variant<std::monostate,Ts...>>>::value) {
+                    return false;
+                } else {
+                    return value_internal<Index+1>();
+                }
+            } else {
+                return true;
+            }
+        }
+    public:
+        static constexpr bool value = value_internal<0>();
+    };
+    template <class... Ts>
+    class JsonEncoder<JsonVariantAutoDetect<Ts...>, std::enable_if_t<JsonWrappable<JsonVariantAutoDetect<Ts...>>::value, void>> {
+    private:
+        template <std::size_t Index>
+        static void write_impl(nlohmann::json &output, JsonVariantAutoDetect<Ts...> const &data) {
+            if constexpr (Index >= 0 && Index < sizeof...(Ts)) {
+                if (Index+1 == data.value.index()) {
+                    using F = std::variant_alternative_t<Index+1,std::variant<std::monostate,Ts...>>;
+                    JsonEncoder<F>::write(
+                        output
+                        , std::nullopt
+                        , std::get<Index+1>(data.value)
+                    );
+                } else {
+                    write_impl<Index+1>(output, data);
+                }
+            }
+        }
+    public:
+        static void write(nlohmann::json &output, std::optional<std::string> const &key, JsonVariantAutoDetect<Ts...> const &data) {
             if constexpr (sizeof...(Ts) >= 0) {
                 write_impl<0>((key?output[*key]:output), data);
             }
@@ -2807,7 +2853,7 @@ namespace dev { namespace cd606 { namespace tm { namespace basic { namespace nlo
         }
     };
     template <class M, class T>
-    class JsonDecoder<SingleLayerWrapperWithTypeMark<M,T>, std::enable_if_t<!std::is_same_v<M,JsonVariantTypeMark>, void>> {
+    class JsonDecoder<SingleLayerWrapperWithTypeMark<M,T>, std::enable_if_t<!std::is_same_v<M,JsonVariantTypeMark> && !std::is_same_v<M,JsonVariantAutoDetectTypeMark>, void>> {
     public:
         static void fillFieldNameMapping(JsonFieldMapping const &mapping=JsonFieldMapping {}) {
             JsonDecoder<T>::fillFieldNameMapping(mapping);
@@ -3050,6 +3096,99 @@ namespace dev { namespace cd606 { namespace tm { namespace basic { namespace nlo
             if (key) {
                 auto x = input.get_object()[*key];
                 if (x.error() == simdjson::NO_SUCH_FIELD) {
+                    return false;
+                }
+                return read_impl_simd_ondemand<0>(x, data, mapping);
+            } else {
+                return read_impl_simd_ondemand<0>(input, data, mapping);
+            }
+        }
+    };
+    template <class... Ts>
+    class JsonDecoder<JsonVariantAutoDetect<Ts...>, std::enable_if_t<JsonWrappable<JsonVariantAutoDetect<Ts...>>::value, void>> {
+    private:
+        template <std::size_t Index>
+        static bool read_impl(nlohmann::json const &input, JsonVariantAutoDetect<Ts...> &data, JsonFieldMapping const &mapping) {
+            if constexpr (Index < sizeof...(Ts)) {
+                using F = std::variant_alternative_t<Index+1, std::variant<std::monostate,Ts...>>;
+                data.value.template emplace<Index+1>();
+                bool res = JsonDecoder<F>::read(input, std::nullopt, std::get<Index+1>(data.value), mapping);
+                if (res) {
+                    return res;
+                } else {
+                    return read_impl<Index+1>(input, data, mapping);
+                }
+            } else {
+                data.value.template emplace<0>();
+                return false;
+            }
+        }
+        template <std::size_t Index>
+        static bool read_impl_simd(simdjson::dom::element const &input, JsonVariantAutoDetect<Ts...> &data, JsonFieldMapping const &mapping) {
+            if constexpr (Index < sizeof...(Ts)) {
+                using F = std::variant_alternative_t<Index+1, std::variant<std::monostate,Ts...>>;
+                data.value.template emplace<Index+1>();
+                bool res = JsonDecoder<F>::read_simd(input, std::nullopt, std::get<Index+1>(data.value), mapping);
+                if (res) {
+                    return res;
+                } else {
+                    return read_impl_simd<Index+1>(input, data, mapping);
+                }
+            } else {
+                data.value.template emplace<0>();
+                return false;
+            }
+        }
+        template <std::size_t Index, class X>
+        static bool read_impl_simd_ondemand(X &input, JsonVariantAutoDetect<Ts...> &data, JsonFieldMapping const &mapping) {
+            if constexpr (Index < sizeof...(Ts)) {
+                using F = std::variant_alternative_t<Index+1, std::variant<std::monostate,Ts...>>;
+                data.value.template emplace<Index+1>();
+                bool res = JsonDecoder<F>::read_simd_ondemand(input, std::nullopt, std::get<Index+1>(data.value), mapping);
+                if (res) {
+                    return res;
+                } else {
+                    return read_impl_simd_ondemand<Index+1,X>(input, data, mapping);
+                }
+            } else {
+                data.value.template emplace<0>();
+                return false;
+            }
+        }
+        template <std::size_t Index>
+        static void fillFieldNameMapping_internal(JsonFieldMapping const &mapping) {
+            if constexpr (Index < sizeof...(Ts)) {
+                using F = std::variant_alternative_t<Index+1, std::variant<std::monostate,Ts...>>;
+                JsonDecoder<F>::fillFieldNameMapping(mapping);
+                fillFieldNameMapping_internal<Index+1>(mapping);
+            }
+        }
+    public:
+        static void fillFieldNameMapping(JsonFieldMapping const &mapping=JsonFieldMapping {}) {
+            fillFieldNameMapping_internal<0>(mapping);
+        }
+        static bool read(nlohmann::json const &input, std::optional<std::string> const &key, JsonVariantAutoDetect<Ts...> &data, JsonFieldMapping const &mapping=JsonFieldMapping {}) {
+            auto const &i = (key?input.at(*key):input);
+            return read_impl<0>(i, data, mapping);
+        }
+        static bool read_simd(simdjson::dom::element const &input, std::optional<std::string> const &key, JsonVariantAutoDetect<Ts...> &data, JsonFieldMapping const &mapping=JsonFieldMapping {}) {
+            if (key) {
+                if (input[*key].error() == simdjson::NO_SUCH_FIELD) {
+                    data.value.template emplace<0>();
+                    return false;
+                }
+                simdjson::dom::element i = input[*key];
+                return read_impl_simd<0>(i, data, mapping);
+            } else {
+                return read_impl_simd<0>(input, data, mapping);
+            }
+        }
+        template <class X>
+        static bool read_simd_ondemand(X &input, std::optional<std::string> const &key, JsonVariantAutoDetect<Ts...> &data, JsonFieldMapping const &mapping=JsonFieldMapping {}) {
+            if (key) {
+                auto x = input.get_object()[*key];
+                if (x.error() == simdjson::NO_SUCH_FIELD) {
+                    data.value.template emplace<0>();
                     return false;
                 }
                 return read_impl_simd_ondemand<0>(x, data, mapping);
